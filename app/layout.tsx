8a25fad5e2af7ddd5d809f3579f7148cd2d0186b
the creator <chenxi.yan@gmail.com>
2023-10-02 09:37:12 -0400

    Halfway Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter, Space_Grotesk } from 'next/font/google'
+import Navbar from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 const spaceGrotesk = Space_Grotesk({
@@ -19,7 +20,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className='max-w-10xl mx-auto bg-gradient-to-br from-amber-100 via-amber-500 to-sky-600'>
+          <Navbar />
+          {children}
+        </main>
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,51 @@
 import React from 'react'
+import Image from 'next/image'
+import Searchbar from '@/components/Searchbar'
+import HeroCarousel from '@/components/HeroCarousel'
 
 const Home = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+    <section className='px-6 md:px-20 py-24'>
+      <div className="flex max-xl:flex-col gap-16">
+        <div className="flex flex-col justify-center">
+          <p className='small-text'>
+            Search, Shop, and Save right now:
+            <Image 
+            src="/assets/icons/arrow-right.svg"
+            alt="arrow-right"
+            width={16}
+            height={16} />
+          </p>
+
+          <h1 className='head-text'>
+            <span className='opacity-80'>Experience the Strength of</span> Price
+            <span className='text-primary'>Me</span>
+          </h1>
+
+          <p className='mt-6 font-bold opacity-70'>
+          Self-serve, powerful product and growth analytics to increase conversion, engagement, and retention.
+          </p>
+
+          <Searchbar />
+        </div>
+
+        <HeroCarousel />
+      </div>
+    </section>
+
+    <section className='trending-section'>
+      <h2 className='section-text'>Trending</h2>
+
+      <div className="flex flex-wrap gap-x-8 gap-y-16">
+        {['Apple Iphone 15', 'Book', 'Sneakers'].map(
+          (product) => (
+            <div>{product}</div>
+          )
+        )}
+      </div>
+    </section>
+    </>
   )
 }
 
